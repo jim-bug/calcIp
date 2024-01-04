61d8e41b5d58b459a6b3b2a402737fbea75b07a7
Ignazio <ignazioandsperandeo@gmail.com>
2024-01-04 16:06:03 +0100

    .

---
+++ calcIp_package/__init__.py
@@ -0,0 +1,35 @@
+from calcIp_package.convertitor.convertitor import dec_to_bin, bin_to_dec
+from calcIp_package.colors.color import Colors
+
+# funzione che trasforma l'ip_address_dec in binario bit per bit:
+def binary_ipv4(ip, lenght_ip=4):
+    ip_result = []
+    for i in range(lenght_ip):
+        generator = dec_to_bin(int(ip[i]))
+        for k in generator:
+            ip_result.append(k)
+    return ip_result
+
+
+# funzione che trasforma la subnetmask da binaria a decimale:
+def decimal_ipv4(subnet_mask, lenght_ip=4):
+    subnet = []
+    start = 0
+    for i in range(lenght_ip):
+        subnet.append(str(bin_to_dec(subnet_mask[start: start+8])))
+        start += 8      # mi sposto di 8 perchè sto estrando byte
+    return subnet
+
+# funzione che manda a video un indirizzo ip binario con la suddivisione dei bit dedicati alla rete e all'host
+def print_ip_bin(ip_bin, subnet_mask_cidr, message, lenght_byte_ip=32):
+    i = 0
+    print(f"{message}: ", end='')
+    for j in range(lenght_byte_ip):
+        if j % 8 == 0 and j != 0:
+            print('.', end='')
+        if i < subnet_mask_cidr:
+            print(Colors.GREEN + str(ip_bin[i]), end='')      # i bit dedicati alla rete li mando a video in verde
+            i += 1
+        else:
+            print(Colors.RED + str(ip_bin[j]), end='')     # i bit dedicati agli host li mando a video in rosso
+    print(Colors.RESET)
---
+++ main.py
@@ -0,0 +1,71 @@
+# Autore: Ignazio Leonardo Calogero Sperandeo
+# Data 03/01/2024
+# Nome progetto: CalcIp
+# by jim_bug :)
+from sys import path
+from argparse import ArgumentParser
+from os import getcwd
+from calcIp_package import binary_ipv4, decimal_ipv4, print_ip_bin
+path.append(getcwd()+'\\calcIp_package')
+# path.append("C:\\Users\\Ignazio\\Desktop\\coding\\project_python\\project\\calcip\\calcIp_package")
+
+
+
+LENGHT_BYTE_IP = 4
+LENGHT_BIT_IP = 32
+parser = ArgumentParser()
+parser.add_argument('ipv4', type=str, help='ipv4 concatenato alla subnet mask in notazione CIDR')
+args = parser.parse_args()
+ip_address_dec = args.ipv4
+subnet_mask_dec = []
+network_address_dec = []
+broadcast_address_dec = []
+subnet_mask_cidr = 0
+point_pos = []
+ip_address_bin = []
+subnet_mask_bin = []
+network_address_bin = []
+broadcast_address_bin = []
+max_host = 0
+start = 0
+
+# divido ip da subnet mask in notazione CIDR
+for i in range(len(ip_address_dec)):
+    if ip_address_dec[i] == '/':
+        subnet_mask_cidr = ip_address_dec[i + 1:]
+        subnet_mask_cidr = int(subnet_mask_cidr)
+        ip_address_dec = ip_address_dec[:i]     # in questa maniera lascio solo l'ip_address_dec senza la subnet mask in notazione CIDR
+        break
+
+j = 0
+for i in range(LENGHT_BIT_IP):   # ricavo la subnetmask in binario
+    if j < subnet_mask_cidr:
+        subnet_mask_bin.append('1')
+        j += 1
+    else:
+        subnet_mask_bin.append('0')
+
+max_host = 2 ** (32 - subnet_mask_cidr) - 2
+ip_address_dec = ip_address_dec.split('.')          # rendo l'ip una lista di stringhe con omessi i punti
+ip_address_bin = binary_ipv4(ip_address_dec)      # trasformo in binario l'ip
+subnet_mask_dec = decimal_ipv4(subnet_mask_bin)  # trasformo in decimale la subnet mask
+
+for i in range(LENGHT_BYTE_IP):
+    network_address_dec.append(str(int(ip_address_dec[i]) & int(subnet_mask_dec[i])))       # operazione and bit a bit tra ip e subnet mask
+network_address_bin = binary_ipv4(network_address_dec)
+
+j = 0
+for i in range(LENGHT_BIT_IP):
+    if j < subnet_mask_cidr:    # finche sono nei bit della rete copio ogni bit
+        broadcast_address_bin.append(str(ip_address_bin[i]))
+        j += 1
+    else:
+        broadcast_address_bin.append('1')   # nei bit dell'host invece li pongo tutti a 1
+broadcast_address_dec = decimal_ipv4(broadcast_address_bin)
+
+print(f"IP: {'.'.join(ip_address_dec)}\nSubnet mask: {'.'.join(subnet_mask_dec)}\nSubnet mask in notazione CIDR: /{subnet_mask_cidr}\nIP rete: {'.'.join(network_address_dec)}\nIP broadcast: {'.'.join(broadcast_address_dec)}\nHost massimi: {max_host}")
+print("---------------------")
+print_ip_bin(ip_address_bin, subnet_mask_cidr, "IP binario")
+print_ip_bin(subnet_mask_bin, subnet_mask_cidr, "Subnet mask binaria")
+print_ip_bin(network_address_bin, subnet_mask_cidr, "IP rete binario")
+print_ip_bin(broadcast_address_bin, subnet_mask_cidr, "IP broadcast binario")
