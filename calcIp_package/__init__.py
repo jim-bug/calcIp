d954742f5461d9d9dc3f91eef3314ac42de2e955
Ignazio <ignazioandsperandeo@gmail.com>
2024-01-04 16:38:18 +0100

    Aggiunto a video la classe di indirizzo di quell'ipv4, risoluzione BUG casting esplicito

--- calcIp_package/__init__.py
+++ calcIp_package/__init__.py
@@ -22,11 +22,12 @@ def decimal_ipv4(subnet_mask, lenght_ip=4):
 
 # funzione che determina la classe di indirizzo di quell'ipv4
 def which_class(ip):
-    if 0 < ip[0] < 127:
+    first_byte = int(ip[0])
+    if 0 < first_byte < 127:
         return 'A'
-    elif 128 < ip[0] < 191:
+    elif 128 < first_byte < 191:
         return 'B'
-    elif 192 < ip[0] < 223:
+    elif 192 < first_byte < 223:
         return 'C'
 
 
