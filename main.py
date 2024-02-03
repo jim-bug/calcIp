fe504baa5bea4a48f7bd98c3e46fa60074fdde35
OttoVolt <123885945+OttoVolt@users.noreply.github.com>
2024-02-03 23:16:44 +0100

    Update main.py
    import os to get os.path.join()

--- main.py
+++ main.py
@@ -4,10 +4,11 @@
 # by jim_bug :)
 from sys import path
 from argparse import ArgumentParser
-from os import getcwd, path
+from os import getcwd
+import os
 from calcIp_package import binary_ipv4, decimal_ipv4, print_ip_bin, which_class, wild_card_calculation
 # https://www.geeksforgeeks.org/python-os-path-join-method/
-path.append(path.join(getcwd(),'calcIp_package'))
+path.append(os.path.join(getcwd(),'calcIp_package'))
 # path.append("C:\\Users\\Ignazio\\Desktop\\coding\\project_python\\project\\calcip\\calcIp_package")
 
 
